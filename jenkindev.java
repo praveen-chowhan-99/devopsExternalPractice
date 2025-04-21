93832d1634f38bc58971e6250fc1108331bc49bf
praveen-chowhan-99 <praveenkorra.office@gmail.com>
2025-04-21 18:23:37 +0530

    changed jenkindev.java

--- jenkindev.java
+++ jenkindev.java
@@ -1,5 +1,6 @@
 public class jenkindev{
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins!");
+        System.out.println("This is a test for Jenkins.");
 }
 }
